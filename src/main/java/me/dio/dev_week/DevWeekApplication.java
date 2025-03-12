0438152fbca453696aabfda89316e7cdc948e75c
Adriel Rudson <adrielrudsonsalgado@gmail.com>
2025-03-12 20:00:58 -0300

    Java RESTful API.

---
+++ src/main/java/me/dio/dev_week/DevWeekApplication.java
@@ -0,0 +1,13 @@
+package me.dio.dev_week;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevWeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevWeekApplication.class, args);
+	}
+
+}
